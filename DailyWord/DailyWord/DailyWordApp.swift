1b16d5844630d422844138bf68fe309ecd2ffc5a
Sharik <sharikmohamed8@gmail.com>
2025-05-29 10:23:55 +0200

    update

---
+++ DailyWord/DailyWord/DailyWordApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyWordApp.swift
+//  DailyWord
+//
+//  Created by Sharik Mohamed on 29/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct DailyWordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DailyWord/DailyWordTests/DailyWordTests.swift
@@ -0,0 +1,17 @@
+//
+//  DailyWordTests.swift
+//  DailyWordTests
+//
+//  Created by Sharik Mohamed on 29/05/2025.
+//
+
+import Testing
+@testable import DailyWord
+
+struct DailyWordTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
